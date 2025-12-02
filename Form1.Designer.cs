2bbeacf66faac15f538a06a12b7e76f12c2e6fa9
Rawisl <trthihoaingoc@gmail.com>
2025-12-02 20:07:55 +0700

    Them tinh nang undo/redo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,12 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.file_menu = new System.Windows.Forms.ToolStripMenuItem();
-            this.open_menu = new System.Windows.Forms.ToolStripMenuItem();
-            this.new_menu = new System.Windows.Forms.ToolStripMenuItem();
-            this.save_menu = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveAsMenu = new System.Windows.Forms.ToolStripMenuItem();
             this.canvasContainer_panel = new System.Windows.Forms.Panel();
             this.toolStrip = new System.Windows.Forms.ToolStrip();
             this.fileButton = new System.Windows.Forms.ToolStripDropDownButton();
@@ -51,7 +45,6 @@
             this.toolsPanel1 = new WinForm_Paint_Gr12.ToolsPanel();
             this.statusPanel1 = new WinForm_Paint_Gr12.StatusPanel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.menuStrip1.SuspendLayout();
             this.canvasContainer_panel.SuspendLayout();
             this.toolStrip.SuspendLayout();
             this.SuspendLayout();
@@ -69,65 +62,6 @@
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.menuStrip1.Enabled = false;
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.file_menu});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 725);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1482, 28);
-            this.menuStrip1.TabIndex = 4;
-            this.menuStrip1.Text = "menuStrip1";
-            this.menuStrip1.Visible = false;
-            // 
-            // file_menu
-            // 
-            this.file_menu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.open_menu,
-            this.new_menu,
-            this.save_menu,
-            this.saveAsMenu});
-            this.file_menu.Name = "file_menu";
-            this.file_menu.Size = new System.Drawing.Size(46, 24);
-            this.file_menu.Text = "&File";
-            this.file_menu.Click += new System.EventHandler(this.file_menu_Click);
-            // 
-            // open_menu
-            // 
-            this.open_menu.Name = "open_menu";
-            this.open_menu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.open_menu.Size = new System.Drawing.Size(233, 26);
-            this.open_menu.Text = "&Open";
-            this.open_menu.Click += new System.EventHandler(this.open_menu_Click);
-            // 
-            // new_menu
-            // 
-            this.new_menu.Name = "new_menu";
-            this.new_menu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-            this.new_menu.Size = new System.Drawing.Size(233, 26);
-            this.new_menu.Text = "&New";
-            this.new_menu.Click += new System.EventHandler(this.new_menu_Click);
-            // 
-            // save_menu
-            // 
-            this.save_menu.Name = "save_menu";
-            this.save_menu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.save_menu.Size = new System.Drawing.Size(233, 26);
-            this.save_menu.Text = "&Save";
-            this.save_menu.Click += new System.EventHandler(this.save_menu_Click);
-            // 
-            // saveAsMenu
-            // 
-            this.saveAsMenu.Name = "saveAsMenu";
-            this.saveAsMenu.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
-            | System.Windows.Forms.Keys.S)));
-            this.saveAsMenu.Size = new System.Drawing.Size(233, 26);
-            this.saveAsMenu.Text = "Save &As";
-            this.saveAsMenu.Click += new System.EventHandler(this.saveAsMenu_Click);
             // 
             // canvasContainer_panel
             // 
@@ -284,12 +218,10 @@
             this.Controls.Add(this.propertiesPanel1);
             this.Controls.Add(this.toolsPanel1);
             this.Controls.Add(this.statusPanel1);
-            this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.toolStrip);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "mainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Paint";
@@ -297,8 +229,6 @@
             this.Load += new System.EventHandler(this.mainForm_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mainForm_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.canvasContainer_panel.ResumeLayout(false);
             this.canvasContainer_panel.PerformLayout();
             this.toolStrip.ResumeLayout(false);
@@ -314,13 +244,7 @@
         private ToolsPanel toolsPanel1;
         private PropertiesPanel propertiesPanel1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem file_menu;
-        private System.Windows.Forms.ToolStripMenuItem open_menu;
-        private System.Windows.Forms.ToolStripMenuItem new_menu;
-        private System.Windows.Forms.ToolStripMenuItem save_menu;
         private System.Windows.Forms.Panel canvasContainer_panel;
-        private System.Windows.Forms.ToolStripMenuItem saveAsMenu;
         private System.Windows.Forms.ToolStrip toolStrip;
         private System.Windows.Forms.ToolStripDropDownButton fileButton;
         private System.Windows.Forms.ToolStripMenuItem openFileButton;
